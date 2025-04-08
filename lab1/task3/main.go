1f507aa3d5b4e20495592cd555428803ab564c98
hrvadl <vadym.hrashchenko@makeheadway.com>
2025-04-08 10:29:51 +0300

    feat(lab1): finished task #3

---
+++ lab1/task3/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func gcd(m, n int) int {
+	if n == 0 {
+		return m
+	}
+
+	return gcd(n, m%n)
+}
+
+func main() {
+	var m, n int
+	fmt.Print("Please, input m and n: ")
+	fmt.Scan(&m, &n)
+
+	result := gcd(m, n)
+	fmt.Printf("НСД(%d, %d) = %d\n", m, n, result)
+}
